ea3742ff1e5e5b70ec6d231072441ee0384071c3
Calvin Magezi <30309553+CalvinMagezi@users.noreply.github.com>
2021-12-22 04:29:40 +0400

    Added preloader

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,26 @@
+import Router from "next/router";
 import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
+import NProgress from "nprogress";
+import Loader from "../components/utils/Loader";
+import { useState } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [loading, setLoading] = useState(false);
+  Router.events.on("routeChangeStart", (url) => {
+    NProgress.start();
+    setLoading(true);
+  });
+  Router.events.on("routeChangeComplete", (url) => {
+    NProgress.done();
+    setLoading(false);
+  });
+  return (
+    <>
+      {loading && <Loader />}
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
